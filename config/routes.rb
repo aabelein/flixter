06424c45b78052ca6066ce1b8b663e2e494ff154
Ariel Abelein <arielabelein@gmail.com>
2021-12-30 22:45:42 +0000

    :fixed new course page

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,10 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:create] 
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
   end
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
